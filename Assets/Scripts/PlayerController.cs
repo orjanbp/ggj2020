1b1e8abf1b4cd468c81048e0f0100849e7d0bb16
Thomas Tyssøy <battlesheep5@gmail.com>
2020-01-31 23:20:11 +0100

    Playercontroller, layers.
    Added playercontroller. Changed tag settings, added attach point layer.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray inputRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit))
+        {
+            if (hit.collider.gameObject.layer == 8)
+            {
+                AttachPoint attachPoint = hit.collider.gameObject.GetComponent<AttachPoint>();
+                if (attachPoint != null)
+                {
+                    Debug.Log("FOUND ATTACH POINT");
+                }
+            }
+        }
+    }
+}
