067c4e3004de4f8374edfe2e4e8132d3a5869e01
orjanp <oerjanbpolden@gmail.com>
2020-01-31 23:26:18 +0100

    mock animal prefab

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttachPoint : MonoBehaviour
+public class Animal : MonoBehaviour
 {
 
-    public string LimbName;
+    public AttachPoint[] LimbCollection;
 
     // Start is called before the first frame update
     void Start()
