78bda0ef7c10f52cbab143bb0855b79f9057a4f7
Ørjan Polden <oerjanbpolden@gmail.com>
2020-02-01 00:41:37 +0100

    setup limb manager

---
+++ Assets/Scripts/LimbManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimbManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Limb[] LimbList;
+
+    public Limb GetRandomLimb()
+    {
+        var randLimb = null; // TODO: Fetch limb
+        return randLimb;
+    }
+
+    public Limb GetRandomHead()
+    {
+        var randHead = null; // TODO: Fetch head
+        return randHead;
+    }
+}
