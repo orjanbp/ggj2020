ce34d31bbc90662b0ddfd8d49a58b21c44e1ead6
Thomas Tyssøy <battlesheep5@gmail.com>
2020-02-01 00:29:39 +0100

    Added draggable script
    draggable script

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+
+    public void OnDragStart()
+    {
+        Debug.Log("On drag start");
+    }
+
+    public void OnDragInDirection(Vector2 direction)
+    {
+        Debug.Log("Draggable DIRECTION " + direction);
+    }
+
+    public void OnDragStop()
+    {
+        Debug.Log("On Drag end");
+    }
+
+}
