68d6ab9475a1e27d7c416122cf90ee1185c94dac
Thomas Tyssøy <battlesheep5@gmail.com>
2020-01-31 22:40:55 +0100

    Added limb scripts and prefabs
    Limb scripts and prefabs.

---
+++ Assets/Scripts/Limb.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limb : MonoBehaviour
+{
+    public Vector3 anchorPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    #if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 1, 0, 0.75F);
+        Vector3 anchorRealWorld = transform.position + anchorPoint;
+        Gizmos.DrawRay(anchorRealWorld, transform.right);
+    }
+    #endif
+}
